9452777bb19f43d6f5f7e9c836c929d98d4af6f8
hugolopesxr <hugo_xr@hotmail.com>
2023-01-16 14:22:35 -0300

    first commit

---
+++ carro.js
@@ -0,0 +1,29 @@
+//codigo do carro
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 5.5];
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+      xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaTela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }  
+}
+
+function passouTodaTela(xCarro){
+   return xCarro < - 50;
+}
